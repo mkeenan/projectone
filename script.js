7eda7b228f592ecfb2bf659bfc9ab16f910c9594
Mary Keenan <marykeenan@Marys-MacBook-Pro.local>
2014-04-25 18:43:29 -0700

    latest update with Firebase

--- script.js
+++ script.js
@@ -1,49 +1,115 @@
+var myGame = angular.module("TTTApp",["firebase"]);
+
+
 //establishes the controller to start the application
-function TicTacToeController($scope) {
-//set initial player to "X"
-  $scope.turn = 1;
-//create empty cells in $scope
-  $scope.cells=[0,0,0,0,0,0,0,0,0];
+function TicTacToeController($scope, $firebase) {
+
+//connect to lcm database through Firebase and add a section called games
+  var TTTRef = new Firebase("https://lcm.firebaseio.com/games");
+
+// //set initial player to "X"
+//   $scope.turn = 1;
+// //create empty cells in $scope
+//   $scope.cells=[0,0,0,0,0,0,0,0,0];
+  $scope.game = {};
+//chunk of code below sets up firebase:
+  var lastGame;
+        // Ask for all existing game info from firebase
+        TTTRef.once('value', function(gamesSnapshot) {
+          // get the actual games data
+          var lastGame;
+          var games = gamesSnapshot.val();
+          if(games == null)
+          {
+            // No games at all, so make a new game -- As if we're Areg
+            lastGame = TTTRef.push( {waiting: true} );
+            playerNum = 1;
+          }
+          else  // I do have at least one game out there...
+          {
+            var keys = Object.keys(games);
+            var lastGameKey = keys[ keys.length - 1 ];
+            lastGame = games[ lastGameKey ];
+            console.log("This person's game: " + lastGameKey);
+            if(lastGame.waiting)
+            {
+              // Currently someone is waiting -- eg: Areg is there and we're Rocky
+              // Grab from Firebase its lasts game object
+              lastGame = TTTRef.child(lastGameKey);
+              // Set a new game on this
+              lastGame.set( {waiting:false, turn: 1, won: false, cells : [0,0,0,0,0,0,0,0,0]} );
+              playerNum = 2;
+            }
+            else
+            {
+              // Make a new game -- As if we're Areg
+              lastGame = TTTRef.push( {waiting: true} );
+              playerNum = 1;
+            }
+          }
+          // Attach the last game to what we're up to
+          fbBind = $firebase(lastGame);
+          fbBind.$bind($scope, "game");
+      });
+
+
 
 //create click as the main function to swap players and X's/O's
   $scope.click = function(i) {
 //only allow clicking of empty spaces
-    if ($scope.cells[i] == 0) {
+    if ($scope.game.cells[i] == 0 &&
+
+//alternate turns
+      (playerNum == 1 && $scope.game.turn == 1 || playerNum == 2 && $scope.game.turn == -1)) {
+
 //mark the move/turn
-      $scope.cells[i] = $scope.turn;
+      $scope.game.cells[i] = $scope.game.turn;
 //use an if statement to swap from X to O for next click and vice-versa
-      if ($scope.turn == 1) {
-        $scope.turn = -1;
+      if ($scope.game.turn == 1) {
+        $scope.game.turn = -1;
       }
       else {
-        $scope.turn = 1;
+        $scope.game.turn = 1;
       }
 //call the gameOver function
      $scope.gameOver();
  //end of click function curly brace
-    }
+    };
  };
 
+   $scope.convert = function (c) {
+    console.log(c);
+    if (c == 0) {
+      return "";
+    }
+    else if (c == 1) {
+      return "";
+    }
+    else {
+      return "";
+    }
+   };
 
   $scope.gameOver = function() {
 //identify win variations
         $scope.win = [
-          [$scope.cells[0], $scope.cells[1], $scope.cells[2]],
-          [$scope.cells[3], $scope.cells[4], $scope.cells[5]],
-          [$scope.cells[6], $scope.cells[7], $scope.cells[8]],
-          [$scope.cells[0], $scope.cells[3], $scope.cells[6]],
-          [$scope.cells[1], $scope.cells[4], $scope.cells[7]],
-          [$scope.cells[2], $scope.cells[5], $scope.cells[8]],
-          [$scope.cells[0], $scope.cells[4], $scope.cells[8]],
-          [$scope.cells[2], $scope.cells[4], $scope.cells[6]]
+          [$scope.game.cells[0], $scope.game.cells[1], $scope.game.cells[2]],
+          [$scope.game.cells[3], $scope.game.cells[4], $scope.game.cells[5]],
+          [$scope.game.cells[6], $scope.game.cells[7], $scope.game.cells[8]],
+          [$scope.game.cells[0], $scope.game.cells[3], $scope.game.cells[6]],
+          [$scope.game.cells[1], $scope.game.cells[4], $scope.game.cells[7]],
+          [$scope.game.cells[2], $scope.game.cells[5], $scope.game.cells[8]],
+          [$scope.game.cells[0], $scope.game.cells[4], $scope.game.cells[8]],
+          [$scope.game.cells[2], $scope.game.cells[4], $scope.game.cells[6]]
           ];
 //for loop to iterate over an array
         for (var i = 0; i < 8; i++) {
 //add the items in the index together to see if they equal 3 or -3, which are both win scenarios, [i]brings you to the array it's looking at [#] shows the item index number being added.
           var sum = $scope.win[i][0] + $scope.win[i][1] + $scope.win[i][2];
           if (sum == 3 || sum == -3) {
+            $scope.$apply();
             alert("you win!");
-            $scope.cells=[0,0,0,0,0,0,0,0,0];
+            $scope.game.cells=[0,0,0,0,0,0,0,0,0];
             break;
           }
 
@@ -53,24 +119,3 @@ function TicTacToeController($scope) {
  //end of controller
 }
 
-
-//Bugs:
-//won't stop saying "you win" after the first win occurs. Possibly add a div around the click function to prevent by showing that cell is not 0.??
-
-//Paul Kim's approach to adding array elements
-  // function seeIfWin(array) {
-  //   sum = array[0] + array[1] + array[2];
-  //   if (sum == 3 || sum == -3) {
-  //     return true;
-  //   }
-  // }
-
-// Win scenarios:
- //         row1 = [$scope.cell[0], $scope.cell[1], $scope.cell[2],]
-        // row2 = [$scope.cell[3], $scope.cell[4], $scope.cell[5],]
-        // row3 = [$scope.cell[6], $scope.cell[7], $scope.cell[8],]
-        // col1 = [$scope.cell[0], $scope.cell[3], $scope.cell[6],]
-        // col2 = [$scope.cell[1], $scope.cell[4], $scope.cell[7],]
-        // col3 = [$scope.cell[2], $scope.cell[5], $scope.cell[8],]
-        // diag1 = [$scope.cell[0], $scope.cell[4], $scope.cell[8],]
-        // diag1 = [$scope.cell[2], $scope.cell[4], $scope.cell[6],]
